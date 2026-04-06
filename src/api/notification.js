e25c14dc78cfa03d0d003877320482cbe8500b62
PUVVADA BHASKAR <2400030295@kluniversity.in>
2026-04-06 09:12:53 +0530

    added files

--- src/api/notification.js
+++ src/api/notification.js
@@ -1,21 +1,27 @@
-import api from "./axios";
+import axios from "axios";
 
+const BASE_URL = "http://localhost:2212/api";
+
+// Send notification
 export const sendNotification = async (data) => {
-  const response = await api.post("/notifications", data);
+  const response = await axios.post(`${BASE_URL}/notifications`, data);
   return response.data;
 };
 
+// Get notifications
 export const getUserNotifications = async (userId) => {
-  const response = await api.get(`/notifications/user/${userId}`);
+  const response = await axios.get(`${BASE_URL}/notifications/user/${userId}`);
   return response.data;
 };
 
+// Mark as read
 export const markAsRead = async (id) => {
-  const response = await api.put(`/notifications/${id}/read`);
+  const response = await axios.put(`${BASE_URL}/notifications/${id}/read`);
   return response.data;
 };
 
+// Delete notification
 export const deleteNotification = async (id) => {
-  const response = await api.delete(`/notifications/${id}`);
+  const response = await axios.delete(`${BASE_URL}/notifications/${id}`);
   return response.data;
 };
